92ef46bb763cbe8ada6dc1caf99bf15a7fcc1609
雨雅 <wenjing.fwj@alibaba-inc.com>
2017-03-30 08:35:21 +0800

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 静静 on 30/3/17.
+//  Copyright © 2017年 jing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
